57f874d0eb65b5c461d07cc921884e38188808a2
smiadv2022 <msorokina@spacemedia.com.ua>
2023-05-20 10:33:03 +0300

    whith out

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      wwwwwwwwwwwwwwwwwww
+      wwwwwww
       {/* <Profile
         username={user.username}
         tag={user.tag}
